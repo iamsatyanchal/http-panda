df234edce3c724ada4edf1ddd0486ec0a97b5704
Satyanchal Chaudhary <62104921+iamsatyanchal@users.noreply.github.com>
2026-02-05 02:00:19 +0530

    Refactor App component and update status code display

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 function App() {
-  const [url, seturl] = useState('chutiya');
   const status_codes = [
     { code: 100, message: "Continue" },
     { code: 101, message: "Switching Protocols" },
@@ -51,7 +49,7 @@ function App() {
     { code: 426, message: "Upgrade Required" },
     { code: 428, message: "Precondition Required" },
     { code: 429, message: "Too Many Requests" },
-    { code: 431, message: "Request Header Fields Too Large" },
+    { code: 431, message: "Req. Header Fields Too Large" },
     { code: 451, message: "Unavailable For Legal Reasons" },
     { code: 500, message: "Internal Server Error" },
     { code: 501, message: "Not Implemented" },
@@ -65,17 +63,29 @@ function App() {
     { code: 510, message: "Not Extended" },
     { code: 511, message: "Network Auth Required" }];
   return (
-    <div className="flex flex-col justify-center items-center bg-green-500">
-      <div className="flex flex-col grid grid-cols-3 gap-6">
+    <div className=" flex flex-col justify-center items-center bg-green-500/90 bg-[url('./Bamboo-PNG-Pic-Background.png')] bg-no-repeat bg-center bg-cover min-h-screen bg-fixed">
+      <div className="text-center py-6">
+        <p className="text-8xl text-white mb-2 drop-shadow-[0_1px_10px_rgba(0,0,0,0.6)]">http~pandas!</p>
+        <p className="text-[28px] mt-5 text-white drop-shadow-[0px_3px_6px_rgba(0,0,0,0.6)]">~ pandas for every http response status code ~</p>
+        <div className="w-full my-4 relative rounded-lg ring-1 ring-gray-900 ring-opacity-5 bg-green-700 block">
+          <pre className="block scrollbar-none overflow-auto text-white text-sm leading-normal">
+            <code className="inline-block my-3 scrolling-touch subpixel-antialiased text-xl">http-pandas.vercel.app/<span className="text-red-400 font-bold">[code]</span>.jpg</code>
+          </pre>
+        </div>
+      </div>
+      <div className="flex flex-col grid grid-cols-3 gap-6 mb-8">
         {status_codes.map((code) => {
           return (
-            <div className="bg-white p-2 w-72 rounded-lg">
+            <div className="bg-white p-2 w-72 rounded-lg border-[2.5px] border-teal-950">
               <span className="absolute px-2 rounded-[0px_0px_6px_0px] text-center bg-white text-lg text-black" key={code.code}>{code.code}</span>
               <img src={`./${code.code}.jpg`} className="w-72 h-72 rounded-[9px_9px_0px_0px] border-[3px] border-white" />
               <p className="text-center px-1 mt-2 text-xl text-black box-shadow-lg" key={code.code}> {code.message}</p>
             </div>
           )
         })}
+      </div>
+      <div className="mb-10 mt-4">
+        <p className="drop-shadow-[0_0px_9px_rgba(0,0,0,0.6)] text-slate-900 text-[23px]">~ Built with lots of 💖 and 🥤 by <span className="relative bottom-[3px] text-[20px] text-white bg-red-500 border-2 border-black rounded-lg px-2 pb-1 pt-0.5">@iamsatyanchal</span> ~</p>
       </div>
     </div>
   )
